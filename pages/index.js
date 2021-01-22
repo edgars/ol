6479d14b26cc65c555e552f2a76633cc61d88cc1
edgars <edgar@skalena.com>
2021-01-21 21:03:35 -0300

    files

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Osvaldo <a href="https://nextjs.org">Lopes</a>
         </h1>
 
         <p className={styles.description}>
